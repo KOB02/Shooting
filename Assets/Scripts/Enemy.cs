bc1d46def907918c1d021da58cad3c4d8b81b1e8
KOB02 <get,smile.0618@gmail.com>
2016-06-22 14:35:24 +0900

    プロジェクトを追加

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	//Spaceshipコンポーネント
+	Spaceship spaceship;
+
+	IEnumerator Start(){
+		//Spaceshipコンポーネントを取得
+		spaceship = GetComponent<Spaceship> ();
+
+		//ローカル座標のY軸のマイナス方向に移動する
+		spaceship.Move (transform.up * -1);
+
+		//canShotがfalseの場合、ここでコルーチンを終了させる
+		if (spaceship.canShot == false) {
+			yield break;
+		}
+
+		while (true) {
+			//子要素をすべて取得する
+			for (int i = 0; i < transform.childCount; i++) {
+				Transform shotPosition = transform.GetChild (i);
+
+				//Shotpositionの位置・角度で作成
+				spaceship.Shot(shotPosition);
+			
+				//shotDelayを待つ
+				yield return new WaitForSeconds (spaceship.shotDelay);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		//レイヤー名を取得
+		string layerName = LayerMask.LayerToName (c.gameObject.layer);
+
+		//レイヤー名がBullet(Player)以外の時は何も行わない
+		if (layerName != "Bullet(Player)") return;
+
+		//弾の削除
+		Destroy (c.gameObject);
+
+		//爆発する
+		spaceship.Explosion ();
+
+		//エネミーの削除
+		Destroy(gameObject);
+	}
+}
