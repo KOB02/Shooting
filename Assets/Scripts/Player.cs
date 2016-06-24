6cf4ddfd34e59bb69e4e89a0dd7cfab6da2e8f8c
KOB02 <get,smile.0618@gmail.com>
2016-06-24 14:35:01 +0900

    タッチ操作に対応

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,9 @@ public class Player : MonoBehaviour {
 	//Spaceshipコンポーネント
 	Spaceship spaceship;
 
-	//Backgroundコンポーネントを取得
-	Background background;
-
 	IEnumerator Start(){
 		//Spaceshipコンポーネントを取得
 		spaceship = GetComponent<Spaceship>();
-
-		//Backgroundコンポーネントを取得。３つのうちどれか一つを取得する
-		background = FindObjectOfType<Background>();
 
 		while (true) {
 			//弾をプレイヤーと同じ位置・角度で作成
@@ -26,31 +20,41 @@ public class Player : MonoBehaviour {
 	}
 
 	void Update () {
-		//右・左
-		float x = Input.GetAxisRaw("Horizontal");
+		Vector2 pos = Vector2.zero;
 
-		//上・下
-		float y = Input.GetAxisRaw("Vertical");
-
-		//移動する向きを決める
-		Vector2 direction = new Vector2(x,y).normalized;
+		#if UNITY_EDITOR
 	
-		//移動
-		Move(direction);
+			Vector3 vec = Input.mousePosition;
+			vec.y += 30;
+			pos = Camera.main.ScreenToWorldPoint(vec);
+		#else 
+			pos = transform.ImagePosition;
+			foreach(Touch Touch int Input.touches){
+				Vector3 Vector2 = Touch.ImagePosition;
+				Vector2.yield return += 30;
+				pos = Camera.main.ScreenToWorldPoint(Vector2);
+		}
+		#endif
+		//画面左下のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+		//画面右上のワールド座標をビューポートから取得
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+	
+		//プレイヤーの位置が画面内に収まるように制限をかける
+		pos.x = Mathf.Clamp (pos.x,min.x,max.x);
+		pos.y = Mathf.Clamp (pos.y,min.y,max.y);
+
+		//制限をかけた値をプレイヤーの位置とする
+		transform.position = pos;
 	}
 
-	void Move(Vector3 direction){
-		//背景のスケール
-		Vector2 scale = background.transform.localScale;
-
-		//背景のスケールから取得
-		Vector2 min = scale * -0.5f;
-
-		//背景のスケールから取得
-		Vector2 max = scale * 0.5f;
-
+	void Move(Vector2 direction){
+		//画面左下のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+		//画面右上のワールド座標をビューポートから取得
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
 		//プレイヤーの座標を取得
-		Vector3 pos = transform.position;
+		Vector2 pos = transform.position;
 
 		//移動量を加える
 		pos += direction * spaceship.speed * Time.deltaTime;
@@ -61,6 +65,7 @@ public class Player : MonoBehaviour {
 
 		//制限をかけた値をプレイヤーの位置とする
 		transform.position = pos;
+
 	}
 
 	//ぶつかった瞬間に呼び出される
